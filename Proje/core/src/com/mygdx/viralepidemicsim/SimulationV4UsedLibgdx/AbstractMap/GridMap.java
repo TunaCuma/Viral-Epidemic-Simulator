00c711332bd63c7ac0eb63686b846101c7a969c2
Ahmet Faik Utku <ahmetfaikutku@gmail.com>
2022-05-06 18:35:04 +0300

    I have written all the vertices and the edges with Emre Akgül
    GridMap class added for vertices and edges of the map.

---
+++ Proje/core/src/com/mygdx/viralepidemicsim/SimulationV4UsedLibgdx/AbstractMap/GridMap.java
@@ -0,0 +1,399 @@
+package com.mygdx.viralepidemicsim.SimulationV4UsedLibgdx.AbstractMap;
+import java.awt.Point;
+import java.util.Arrays;
+
+public class GridMap {
+
+    public GridMap() {
+        fillVertices();
+        fillGraph();
+    }
+
+    Point[] vertices = new Point[157];
+    int[][] gridMap = new int[157][157];
+
+    public void fillVertices() {
+        vertices[0] = new Point(295,152);
+        vertices[1] = new Point(405,45); 
+        vertices[2] = new Point(570,45); 
+        vertices[3] = new Point(735,45); 
+        vertices[4] = new Point(900,45); 
+        vertices[5] = new Point(1100,45); 
+        vertices[6] = new Point(1480,160); 
+        vertices[7] = new Point(1540,200); 
+        vertices[8] = new Point(405,260); 
+        vertices[9] = new Point(570,260); 
+        vertices[10] = new Point(735,260);
+        vertices[11] = new Point(900,260);
+        vertices[12] = new Point(1100,260);
+        vertices[13] = new Point(100,480);
+        vertices[14] = new Point(355,490);
+        vertices[15] = new Point(570,320);
+        vertices[16] = new Point(830,400);
+        vertices[17] = new Point(1150,320);
+        vertices[18] = new Point(1350,320);
+        vertices[19] = new Point(1350,530);
+        vertices[20] = new Point(1540,500);
+        vertices[21] = new Point(100,545);
+        vertices[22] = new Point(295,700);
+        vertices[23] = new Point(355,600);
+        vertices[24] = new Point(770,650);
+        vertices[25] = new Point(900,590);
+        vertices[26] = new Point(1150,590);
+        vertices[27] = new Point(1490,590);
+        vertices[28] = new Point(100,840);
+        vertices[29] = new Point(600,840);
+        vertices[30] = new Point(1145,840);
+        vertices[31] = new Point(300,152);
+        vertices[32] = new Point(305,152);
+        vertices[33] = new Point(405,40);
+        vertices[34] = new Point(405,35);
+        vertices[35] = new Point(570,40);
+        vertices[36] = new Point(570,35);
+        vertices[37] = new Point(735,40);
+        vertices[38] = new Point(735,35);
+        vertices[39] = new Point(900,40);
+        vertices[40] = new Point(900,35);
+        vertices[41] = new Point(1100,40);
+        vertices[42] = new Point(1485,160);
+        vertices[43] = new Point(1490,160);
+        vertices[44] = new Point(1535,200);
+        vertices[45] = new Point(1530,200);
+        vertices[46] = new Point(405,265);
+        vertices[47] = new Point(405,270);
+        vertices[48] = new Point(570,265);
+        vertices[49] = new Point(570,270);
+        vertices[50] = new Point(735,265);
+        vertices[51] = new Point(735,270);
+        vertices[52] = new Point(900,265);
+        vertices[53] = new Point(900,270);
+        vertices[54] = new Point(1100,265);
+        vertices[55] = new Point(1100,270);
+        vertices[56] = new Point(100,485);
+        vertices[57] = new Point(100,490);
+        vertices[58] = new Point(350,490);
+        vertices[59] = new Point(345,490);
+        vertices[60] = new Point(570,315);
+        vertices[61] = new Point(570,310);
+        vertices[62] = new Point(825,400);
+        vertices[63] = new Point(820,400);
+        vertices[64] = new Point(1150,315);
+        vertices[65] = new Point(1150,310);
+        vertices[66] = new Point(1350,315);
+        vertices[67] = new Point(1350,310);
+        vertices[68] = new Point(1350,535);
+        vertices[69] = new Point(1350,540);
+        vertices[70] = new Point(1535,500);
+        vertices[71] = new Point(1530,500);
+        vertices[72] = new Point(100,540);
+        vertices[73] = new Point(100,535);
+        vertices[74] = new Point(300,700);
+        vertices[75] = new Point(305,700);
+        vertices[76] = new Point(350,600);
+        vertices[77] = new Point(345,600);
+        vertices[78] = new Point(775,650);
+        vertices[79] = new Point(780,650);
+        vertices[80] = new Point(900,585);
+        vertices[81] = new Point(900,580);
+        vertices[82] = new Point(1150,585);
+        vertices[83] = new Point(1150,580);
+        vertices[84] = new Point(1490,585);
+        vertices[85] = new Point(1490,580);
+        vertices[86] = new Point(100,845);
+        vertices[87] = new Point(100,850);
+        vertices[88] = new Point(600,845);
+        vertices[89] = new Point(600,850);
+        vertices[90] = new Point(1145,845);
+        vertices[91] = new Point(1145,850);
+        vertices[92] = new Point(35,35);
+        vertices[93] = new Point(40,40);
+        vertices[94] = new Point(305,35);
+        vertices[95] = new Point(300,40);
+        vertices[96] = new Point(35,490);
+        vertices[97] = new Point(40,485);
+        vertices[98] = new Point(300,485);
+        vertices[99] = new Point(305,490);
+        vertices[100] = new Point(350,40);
+        vertices[101] = new Point(345,35);
+        vertices[102] = new Point(1485,40);
+        vertices[103] = new Point(1490,35);
+        vertices[104] = new Point(1535,40);
+        vertices[105] = new Point(1530,35);
+        vertices[106] = new Point(1775,40);
+        vertices[107] = new Point(1780,35);
+        vertices[108] = new Point(1535,265);
+        vertices[109] = new Point(1530,270);
+        vertices[110] = new Point(1775,265);
+        vertices[111] = new Point(1780,270);
+        vertices[112] = new Point(350,265);
+        vertices[113] = new Point(345,270);
+        vertices[114] = new Point(1485,265);
+        vertices[115] = new Point(1490,270);
+        vertices[116] = new Point(345,310);
+        vertices[117] = new Point(350,315);
+        vertices[118] = new Point(775,315);
+        vertices[119] = new Point(780,310);
+        vertices[120] = new Point(825,315);
+        vertices[121] = new Point(820,310);
+        vertices[122] = new Point(1485,315);
+        vertices[123] = new Point(1490,310);
+        vertices[124] = new Point(1535,315);
+        vertices[125] = new Point(1530,310);
+        vertices[126] = new Point(1775,315);
+        vertices[127] = new Point(1780,310);
+        vertices[128] = new Point(40,540);
+        vertices[129] = new Point(35,535);
+        vertices[130] = new Point(300,540);
+        vertices[131] = new Point(305,535);
+        vertices[132] = new Point(825,535);
+        vertices[133] = new Point(820,540);
+        vertices[134] = new Point(1485,535);
+        vertices[135] = new Point(1490,540);
+        vertices[136] = new Point(1535,535);
+        vertices[137] = new Point(1530,540);
+        vertices[138] = new Point(1775,535);
+        vertices[139] = new Point(1780,540);
+        vertices[140] = new Point(40,845);
+        vertices[141] = new Point(35,850);
+        vertices[142] = new Point(300,845);
+        vertices[143] = new Point(305,850);
+        vertices[144] = new Point(350,845);
+        vertices[145] = new Point(345,850);
+        vertices[146] = new Point(775,845);
+        vertices[147] = new Point(780,850);
+        vertices[148] = new Point(825,585);
+        vertices[149] = new Point(820,580);
+        vertices[150] = new Point(1775,585);
+        vertices[151] = new Point(1780,580);
+        vertices[152] = new Point(825,845);
+        vertices[153] = new Point(820,850);
+        vertices[154] = new Point(1775,845);
+        vertices[155] = new Point(1780,850);
+        vertices[156] = new Point(1100,35);
+    }
+
+    public void fillGraph(){
+        addEdge(0,31 );
+        addEdge(0,32 );
+        addEdge(1, 33);
+        addEdge(1, 34);
+        addEdge(2, 35);
+        addEdge(2, 36);
+        addEdge(3, 37,38);
+        addEdge(4, 39,40);
+        addEdge(5, 41,156);
+        addEdge(6, 42,43);
+        addEdge(7, 44,45);
+        addEdge(8, 46,47);
+        addEdge(9, 48,49);
+        addEdge(10, 50,51);
+        addEdge(11, 52,53);
+        addEdge(12, 54,55);
+        addEdge(13, 56,57);
+        addEdge(14, 58,59);
+        addEdge(15, 60,61);
+        addEdge(16, 62,63);
+        addEdge(17, 64,65);
+        addEdge(18, 66,67);
+        addEdge(19, 68,69);
+        addEdge(20, 70,71);
+        addEdge(21, 72,73);
+        addEdge(22, 74,75);
+        addEdge(23, 76,77);
+        addEdge(24, 78,79);
+        addEdge(25, 80,81);
+        addEdge(26, 82,83);
+        addEdge(27, 84,85);
+        addEdge(28, 86,87);
+        addEdge(29, 88,89);
+        addEdge(30, 90,91);
+        addEdge(31, 95,0,32);
+        addEdge(32, 31,94 );
+        addEdge(33, 1,34,35);
+        addEdge(34, 33,101);
+        addEdge(35, 2,36,37);
+        addEdge(36, 35,34);
+        addEdge(37, 3,38,39);
+        addEdge(38, 37,36);
+        addEdge(39, 40,4,41);
+        addEdge(40, 39,38);
+        addEdge(41, 5,156,102);
+        addEdge(42, 6,43,114 );
+        addEdge(43, 42, 103);
+        addEdge(44, 7, 45,104);
+        addEdge(45, 44, 109);
+        addEdge(46, 47,8 ,12);
+        addEdge(47, 46, 49);
+        addEdge(48, 49,9,46);
+        addEdge(49, 48,51);
+        addEdge(50, 51,10,48 );
+        addEdge(51, 50, 53);
+        addEdge(52, 53,11,50 );
+        addEdge(53, 52,55);
+        addEdge(54, 55,12, 52);
+        addEdge(55, 54, 111);
+        addEdge(56, 57,13,97);
+        addEdge(57, 56,99);
+        addEdge(58, 14,59,117);
+        addEdge(59, 58,77);
+        addEdge(60, 61,15,118);
+        addEdge(61,60, 116);
+        addEdge(62, 63, 16,120 );
+        addEdge(63, 62, 133 );
+        addEdge(64, 65, 17, 66);
+        addEdge(65, 64, 121);
+        addEdge(66, 67, 122, 18 );
+        addEdge(67, 66, 65);
+        addEdge(68, 69, 19, 132);
+        addEdge(69, 68, 135);
+        addEdge(70, 71, 20, 124);
+        addEdge(71, 70, 137);
+        addEdge(72, 73, 21, 130);
+        addEdge(73, 72, 129);
+        addEdge(74, 75, 22, 142);
+        addEdge(75, 74, 131);
+        addEdge(76, 77, 23, 58);
+        addEdge(77, 76, 145);
+        addEdge(78, 79, 24, 146 );
+        addEdge(79, 78, 119);
+        addEdge(80, 81, 25, 82);
+        addEdge(81, 80, 149);
+        addEdge(82, 83, 26, 84);
+        addEdge(83, 82, 81);
+        addEdge(84, 85, 27, 150);
+        addEdge(85, 84, 83);
+        addEdge(86, 87, 28, 140);
+        addEdge(87, 86, 143);
+        addEdge(88, 89, 29, 144);
+        addEdge(89, 88, 147);
+        addEdge(90, 91, 30, 152);
+        addEdge(91, 90, 155);
+        addEdge(92, 96);
+        addEdge(93, 95);
+        addEdge(94, 92, 101);
+        addEdge(95, 31, 100);
+        addEdge(96, 57, 129);
+        addEdge(97, 93, 128);
+        addEdge(98, 56, 130);
+        addEdge(99, 32, 131);
+        addEdge(100, 33, 102);
+        addEdge(101, 113);
+        addEdge(102, 42, 114, 104);
+        addEdge(103, 156, 40, 101, 105, 38, 36, 34);
+        addEdge(104, 106, 102);
+        addEdge(105, 103, 109, 45);
+        addEdge(106, 110);
+        addEdge(107, 105);
+        addEdge(108, 114, 124, 104);
+        addEdge(109, 115, 125, 111);
+        addEdge(110, 126, 108);
+        addEdge(111, 127, 107);
+        addEdge(112, 117);
+        addEdge(113, 116, 47 , 49, 51 , 53, 55, 115);
+        addEdge(114, 108, 122, 46, 48, 50, 52, 54);
+        addEdge(114, 112);
+        addEdge(115, 109,123,43,103);
+        addEdge(116, 113,59,77,145);
+        addEdge(117, 112,60,118);
+        addEdge(118, 120,78,146);
+        addEdge(119, 121,61,116);
+        addEdge(120, 118,64,66,122);
+        addEdge(121, 119,133);
+        addEdge(122, 114,124,134);
+        addEdge(123, 115,125,67,65,121);
+        addEdge(124, 114,122,126);
+        addEdge(125, 109,123,71,137);
+        addEdge(126, 110,138);
+        addEdge(127, 111,125);
+        addEdge(128, 97,72,130);
+        addEdge(129, 96,73,131);
+        addEdge(130, 74,98);
+        addEdge(131, 73,99);
+        addEdge(132, 62,148);
+        addEdge(133, 69,149);
+        addEdge(134, 68,136);
+        addEdge(135, 123,137);
+        addEdge(136, 70,134);
+        addEdge(137, 139,135);
+        addEdge(138, 136,150);
+        addEdge(139, 127,151);
+        addEdge(140, 128);
+        addEdge(141, 87);
+        addEdge(142, 86);
+        addEdge(143, 75);
+        addEdge(144, 76);
+        addEdge(145, 89);
+        addEdge(146, 88,152);
+        addEdge(147, 79,153);
+        addEdge(148, 80,132);
+        addEdge(149, 153,133);
+        addEdge(150, 154,138);
+        addEdge(151, 85,139);
+        addEdge(152, 148,146);
+        addEdge(153, 91,147);
+        addEdge(154, 90);
+        addEdge(155, 151);
+        addEdge(156, 41,40);
+    }
+
+    public void addEdge(int indexFirst, int indexSecond) {
+        this.gridMap[indexFirst][indexSecond] =
+        (int)(Point.distance(vertices[indexFirst].getX(), vertices[indexFirst].getY(), vertices[indexSecond].getX(), vertices[indexSecond].getY()));
+    }
+    public void addEdge(int indexFirst, int indexSecond, int indexThird) {
+        addEdge(indexFirst, indexSecond);
+        addEdge(indexFirst, indexThird);
+    }
+
+    public void addEdge(int indexFirst, int indexSecond, int indexThird, int indexFourth) {
+        addEdge(indexFirst, indexSecond);
+        addEdge(indexFirst, indexThird);
+        addEdge(indexFirst, indexFourth);
+    }
+
+    public void addEdge(int indexFirst, int indexSecond, int indexThird, int indexFourth, int indexFifth) {
+        addEdge(indexFirst, indexSecond);
+        addEdge(indexFirst, indexThird);
+        addEdge(indexFirst, indexFourth);
+        addEdge(indexFirst, indexFifth);
+    }
+
+    public void addEdge(int indexFirst, int indexSecond, int indexThird, int indexFourth, int indexFifth, int indexSixth) {
+        addEdge(indexFirst, indexSecond);
+        addEdge(indexFirst, indexThird);
+        addEdge(indexFirst, indexFourth);
+        addEdge(indexFirst, indexFifth);
+        addEdge(indexFirst, indexSixth);
+    }
+
+    public void addEdge(int indexFirst, int indexSecond, int indexThird, int indexFourth, int indexFifth, int indexSixth, int indexSeventh) {
+        addEdge(indexFirst, indexSecond);
+        addEdge(indexFirst, indexThird);
+        addEdge(indexFirst, indexFourth);
+        addEdge(indexFirst, indexFifth);
+        addEdge(indexFirst, indexSixth);
+        addEdge(indexFirst, indexSeventh);
+    }
+
+    public void addEdge(int indexFirst, int indexSecond, int indexThird, int indexFourth, int indexFifth, int indexSixth, int indexSeventh, int indexEighth) {
+        addEdge(indexFirst, indexSecond);
+        addEdge(indexFirst, indexThird);
+        addEdge(indexFirst, indexFourth);
+        addEdge(indexFirst, indexFifth);
+        addEdge(indexFirst, indexSixth);
+        addEdge(indexFirst, indexSeventh);
+        addEdge(indexFirst, indexEighth);
+    }
+
+
+    public static void main(String[] args) {
+        GridMap asd = new GridMap();
+        for (int i = 0; i<157; i++) {
+            System.out.println(Arrays.toString(asd.gridMap[i]));
+        }
+        System.out.println();
+    }
+
+
+    
+}
