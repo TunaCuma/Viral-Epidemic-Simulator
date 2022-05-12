fd749f6a26f473e3d78dcd691cdb02dd04826512
Ahmet Faik Utku <ahmetfaikutku@gmail.com>
2022-05-12 05:38:23 +0300

    added comments

--- Proje/core/src/com/mygdx/viralepidemicsim/SimulationV4UsedLibgdx/AbstractMap/Tester.java
+++ Proje/core/src/com/mygdx/viralepidemicsim/SimulationV4UsedLibgdx/AbstractMap/Tester.java
@@ -10,9 +10,9 @@ public class Tester {
     public static void main(String[] args) {
         
         Point emre = new Point(10,10);
-        Point rıfat = new Point(15,10);
+        Point rifat = new Point(15,10);
 
-        System.out.println(emre.distance(rıfat));
+        System.out.println(emre.distance(rifat));
 
         String str = "Faik";
 
--- Proje/core/src/com/mygdx/viralepidemicsim/SimulationV4UsedLibgdx/Building/Building.java
+++ Proje/core/src/com/mygdx/viralepidemicsim/SimulationV4UsedLibgdx/Building/Building.java
@@ -8,6 +8,7 @@ import com.mygdx.viralepidemicsim.SimulationV4UsedLibgdx.Helpers.GameInfo;
 import com.mygdx.viralepidemicsim.SimulationV4UsedLibgdx.Person.Person;
 public class Building {
     
+    //instantiative variables
     Point location;
     int numberOfPerson;
     String name;
@@ -20,6 +21,11 @@ public class Building {
     //double size;
     ArrayList<Person> persons = new ArrayList<Person>(); 
 
+    /**
+     * @param name name of the building
+     * @param location location of the building in the map
+     * @param size size of the building
+     */
     public Building(String name, Point location, double size){
         this.name = name;
         this.location = location;
@@ -28,6 +34,9 @@ public class Building {
         rand =  new Random();
     }
 
+    /**
+     * @return the number of infected people in the building
+     */
     public int getInfectedCount() {
         int counter = 0;
         for (int i = 0; i<persons.size(); i++) {
@@ -38,6 +47,9 @@ public class Building {
         return counter;
     }
 
+    /**
+     * rolls the dice and decides if someone in the building will get exposed
+     */
     public void rollDice() {
         int num = GameInfo.randomBetween(1, (int)size);
         if (num < this.coefficient * persons.size() * persons.size() * getInfectedCount()) {
@@ -45,11 +57,18 @@ public class Building {
         }
     }
 
+    /**
+     * gets a randomly selected person in the building exposed
+     */
     public void getRandomExposed() {
         int a = GameInfo.randomBetween(0, persons.size());
         persons.get(a).getExposed();
     }
 
+    /**
+     * adds a person into the building's Person[]
+     * @param person person that will be added
+     */
     public void addPerson(Person person) {
         persons.add(person);
         person.isInBuilding = true;
@@ -59,6 +78,10 @@ public class Building {
         }
     }
 
+    /**
+     * removes a person from the building's Person[]
+     * @param person the person that will be removed.
+     */
     public void removePerson(Person person) {
         persons.remove(person);
         person.isInBuilding = false;
