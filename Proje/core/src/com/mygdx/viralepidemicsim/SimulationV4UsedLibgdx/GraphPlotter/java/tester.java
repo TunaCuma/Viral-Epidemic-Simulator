9877bc622173386f51a84d7041622b18bc1d4aaf
Emre <Emreakgulcs@gmail.com>
2022-05-12 05:29:22 +0300

    I worked on plotting graph
    I hope it works

---
+++ Proje/core/src/com/mygdx/viralepidemicsim/SimulationV4UsedLibgdx/GraphPlotter/java/tester.java
@@ -0,0 +1,9 @@
+package com.mygdx.viralepidemicsim.SimulationV4UsedLibgdx.GraphPlotter.java;
+
+public class tester {
+    public static void main(String[] args) {
+        CreateFile.createFile();
+        WriteToFile.writeToFile();
+        
+    }
+}
